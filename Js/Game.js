e632b45564a296dcfff571e359f6312d74c00f32
AliAghayev132 <93966817+AliAghayev132@users.noreply.github.com>
2021-12-30 01:27:59 +0400

    Game

--- Js/Game.js
+++ Js/Game.js
@@ -1,13 +1,16 @@
-import Box from './Library/box.js'
+import  Box from './Library/box.js'
 export default class Game{
 
 	player = new Box(100,100,100,100);
+	
+	move(){
 
+	}
 	update(dt){
-		console.log("dt")
+		this.player.setTexture('/HTML-CSS-JS-GAME/Resources/test.png')
 	}
 	render(dt){
 
-
+		this.player.render();
 	}
 }
--- Js/Library/box.js
+++ Js/Library/box.js
@@ -1,24 +1,27 @@
 export default class Box {
-	
+
+	#box = document.createElement("div");
 	#width;
 	#height;
 	#x;
 	#y;
+	#color;
 
-	constructor(width,height,x,y){
+	constructor(width,height,x,y,color = "none"){
 		this.#width = width;
 		this.#height = height;
 		this.#x = x;
 		this.#y = y;
+		this.#color = color;
 
-		let box = document.createElement("div");
-		box.style.position = "absoulate";
-		box.style.left = x  + "px";
-		box.style.top = y + "px";	
-		box.style.width = width + "px";
-		box.style.height = height + "px";
-		box.style.backgroundColor = "black";
-		document.body.appendChild(box);
+		this.#box.style.position = "absoulate";
+		this.#box.style.left = x  + "px";
+		this.#box.style.top = y + "px";	
+		this.#box.style.width = width + "px";
+		this.#box.style.height = height + "px";
+		this.#box.style.backgroundColor = this.#color;
+		this.#box.style.display = "none";
+		document.body.appendChild(this.#box);
 	}
 
 	setPositions(x,y){
@@ -36,5 +39,13 @@ export default class Box {
 		this.#width=width;
 		this.#height=height;
 	}
-
+	setColor(par){
+		this.#color = par;
+	}
+	setTexture(path){
+		this.#box.style.backgroundImage = "url("+path+")";
+	}
+	render(){
+		this.#box.style.display ="block";
+	}
 }
